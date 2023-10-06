85330156bfb26489c0b11caddbdcca42bf58978a
imdonghyun <donghyunlim@postech.ac.kr>
2023-10-06 17:41:14 +0900

    20231006 update

--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 import numpy as np
 
 def calc(h):
-    theta1 = np.arccos((h**2 - 44)/(20*h))
-    theta2 = np.arccos((h**2 + 44)/(24*h))
-    theta3 = theta1 + theta2
-
+    theta1 = np.arccos(h/20)
+    theta2 = 2*theta1
     L1 = 10
     L2 = 3
     L3 = 10
@@ -14,14 +12,14 @@ def calc(h):
     B = L1/L4
     C = L1/L2
 
-    a = A - B*np.cos(theta3) + np.cos(theta3) - C
-    b = 2*np.sin(theta3)
-    c = A - B*np.cos(theta3) - np.cos(theta3) + C
+    a = A - B*np.cos(theta2) + np.cos(theta2) - C
+    b = 2*np.sin(theta2)
+    c = A - B*np.cos(theta2) - np.cos(theta2) + C
 
-    theta4 = 2*np.arctan((-b + np.sqrt(b**2 - 4*a*c))/(2*a))
+    theta3 = 2*np.arctan((-b + np.sqrt(b**2 - 4*a*c))/(2*a))
     
     theta1=theta1*180/np.pi
-    theta4=theta4*180/np.pi
+    theta3=theta3*180/np.pi
 
-    return (theta1, theta4)
+    return (theta1, theta3)
 
